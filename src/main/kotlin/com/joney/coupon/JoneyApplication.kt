997fcfd7130c0e2b33f28ff765b58292a680680c
hoonjo123 <johoon030@gmail.com>
2026-09-08 17:51:57 +0900

    feat: 빌드

---
+++ src/main/kotlin/com/joney/coupon/JoneyApplication.kt
@@ -0,0 +1,11 @@
+package com.joney.coupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JoneyApplication
+
+fun main(args: Array<String>) {
+    runApplication<JoneyApplication>(*args)
+}
---
+++ src/test/kotlin/com/joney/coupon/JoneyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joney.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JoneyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
